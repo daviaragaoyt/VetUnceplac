26cc6f25a6e637076b207d15115cf09c495265c1
DAVI <daviaragaoyt@gmail.com>
2023-08-29 13:59:59 -0300

    Botão q muda de cor

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //alert("Obrigado por acessar");
 
+const chk = document.getElementById('chk')
+
+const headerElement = document.getElementsByClassName('header');
+
+
+
+chk.addEventListener('change', () => {
+    document.body.classList.toggle('dark')
+})  
