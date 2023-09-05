2fe4860a633e503bbdcb177839e08df77fc340de
DAVI <daviaragaoyt@gmail.com>
2023-09-05 15:36:21 -0300

    update

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 //alert("Obrigado por acessar");
 
+async function getApi() {
+    const respostaDaApi = await fetch('https://baco-api.up.railway.app/items').then((e) => {
+        console.log('Galinha:', e)
+    })
+}
+
+getApi()
+
 const chk = document.getElementById('chk')
 
 const headerElement = document.getElementsByClassName('header');
