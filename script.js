6e01625bb7f28c5d595d092161ae515c9b41cce0
DAVI <daviaragaoyt@gmail.com>
2023-09-12 13:16:03 -0300

    JS alterado

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 //alert("Obrigado por acessar");
 
-async function getApi() {
-    const respostaDaApi = await fetch('https://baco-api.up.railway.app/items', {
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-        }
-    }).then((e) => {
-        console.log('Galinha:', e)
-    })
-}
+// async function getApi() {
+//     const respostaDaApi = await fetch('https://baco-api.up.railway.app/items', {
+//         headers: {
+//             "Access-Control-Allow-Origin": "*",
+//         }
+//     }).then((e) => {
+//         console.log('Galinha:', e)
+//     })
+// }
 
-getApi()
+// getApi()
 
 const chk = document.getElementById('chk')
 
